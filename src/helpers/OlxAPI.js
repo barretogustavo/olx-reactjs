c9845762e7865b6e17668fbc793ca420eb90a8df
Gustavo Barreto <gbarreto.alves2@gmail.com>
2021-08-12 17:14:19 -0300

    Login page and requests added

---
+++ src/helpers/OlxAPI.js
@@ -0,0 +1,11 @@
+
+const OlxAPI = {
+
+    login:async (email, password)=>{
+        //consulta ao web service
+        return{error:'Funcionalidade Incompleta'};
+    }
+
+};
+
+export default () => OlxAPI;
