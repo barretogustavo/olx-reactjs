e3a5bf64bbf1641a96bae9a343fc45e1bcfec4dc
Gustavo Barreto <gbarreto.alves2@gmail.com>
2021-08-12 13:13:06 -0300

    OLX project

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+export default ()=>{
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route exact path="/about">
+                <About />
+            </Route>
+            <Route>
+                <NotFound />
+            </Route>
+        </Switch>
+    );
+}
