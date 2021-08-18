246feef8395f20c63b1bc11e6e279bed08629242
Gustavo Barreto <gbarreto.alves2@gmail.com>
2021-08-18 19:47:21 -0300

    Atualizei o link para consumo da API

--- src/helpers/OlxAPI.js
+++ src/helpers/OlxAPI.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 import qs from 'qs';
 
-const BASEAPI = 'http://localhost:3001';
+const BASEAPI = 'http://alunos.b7web.com.br:501';
 
 const apiFetchFile = async (endpoint, body) => {
     if(!body.token) {
